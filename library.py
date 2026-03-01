e0ef4d701ee115d7090e5021f6b089403a62c065
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-01 15:47:47 -0500

    added

--- library.py
+++ library.py
@@ -4,4 +4,7 @@ print(math.pi)
 import random
 print(random.randint(1,100))
 print(random.choice(['banana','apple','cherry','kiwi']))
-
+import json
+data={'name':'prema','age':23}
+json_str=json.dumps(data)
+print(json_str)
