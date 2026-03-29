d88330c121a4d02d2c2e2c902125335fefc9d926
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-29 15:48:32 -0400

    new project

---
+++ pro.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+data={
+    'product':['laptop','tv','mobile','milk','veg','grains'],
+    'price':[1000,467,675,65,65,54]
+
+}
+df=pd.DataFrame(data)
+print(df)
